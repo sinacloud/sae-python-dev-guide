2f7632fff241bbd93afdc75e33b549a89a0f536a
Jaime Chen <nkchenz@gmail.com>
2011-12-22 22:02:57 +0800

    Add local bundle tool for virtualenv

--- dev_server/setup.py
+++ dev_server/setup.py
@@ -8,7 +8,8 @@ setup(
     author_email = "chenzheng2@staff.sina.com.cn",
     description = ("SAE Python development server"),
     install_requires = [
-        'Werkzeug', 
+        'Werkzeug',
+        'pip',
         #'Django==1.2.7',
         #'bottle==0.9.6',
         #'tornado==2.1.1',
