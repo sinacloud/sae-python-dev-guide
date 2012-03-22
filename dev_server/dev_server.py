5f7084563cf6abc6be0adc06c1b7606c8e5f674d
Alan Chan <chanfung032@gmail.com>
2012-03-22 18:57:37 +0800

    Add Mysql supports for dev_server

--- dev_server/dev_server.py
+++ dev_server/dev_server.py
@@ -7,37 +7,74 @@ Make sure you're use python 2.6 for developing
 """
 import sys
 import os
+import os.path
+import re
 import imp
+import yaml
 from werkzeug.serving import run_simple
 from optparse import OptionParser
 
-import sae.core
+def setup_sae_environ(options):
+    # Add dummy pylibmc module
+    import sae.memcache
+    sys.modules['pylibmc'] = sae.memcache
 
-import sae.memcache
-sys.modules['pylibmc'] = sae.memcache
+    # Add app_root to sys.path
+    cwd = os.getcwd()
+    if cwd not in sys.path:
+        sys.path.insert(0, cwd)
 
-def main(app_root, options):
+    conf_path = os.path.join(cwd, 'config.yaml')
+    conf = yaml.load(open(conf_path, "r"))
+    appname = conf['name']
+    appversion = str(conf['version'])
 
+    if options.mysql:
+        import sae.const
+
+        p = re.compile('^(.+):(.+)@(.+):(\d+)$')
+        m = p.match(options.mysql)
+        if not m:
+            raise Exception("Invalid mysql configuration")
+
+        user, password, host, port = m.groups()
+
+        sae.const.MYSQL_DB = 'app_' + appname
+        sae.const.MYSQL_USER = user
+        sae.const.MYSQL_PASS = password
+        sae.const.MYSQL_PORT = port
+        sae.const.MYSQL_HOST = host
+    else:
+        print 'MySQL config not found'
+
+    # Add custom environment variable
     os.environ['HTTP_HOST'] = 'localhost:%d' % options.port
+    os.environ['APP_NAME'] = appname
+    os.environ['APP_VERSION'] = appversion
+
+def main(options):
+    setup_sae_environ(options)
 
     try:
         index = imp.load_source('index', 'index.wsgi')
     except IOError:
         print "Seems you don't have an index.wsgi"
-        sys.exit(-1)
+        return
 
     if not hasattr(index, 'application'):
         print "application not found in index.wsgi"
-        sys.exit(-1)
+        return
 
     if not callable(index.application):
         print "application is not a callable"
-        sys.exit(-1)
+        return
 
-    statics = { '/static': os.path.join(app_root,  'static'),
-              '/media': os.path.join(app_root,  'media'),
-             '/favicon.ico': os.path.join(app_root,  'favicon.ico'),
-             }
+    app_root = os.getcwd()
+    statics = {
+        '/static': os.path.join(app_root,  'static'),
+        '/media': os.path.join(app_root,  'media'),
+        '/favicon.ico': os.path.join(app_root,  'favicon.ico'),
+    }
 
     # FIXME: All files under current directory
     files = ['index.wsgi']
@@ -48,24 +85,15 @@ def main(app_root, options):
                     use_debugger = True,
                     extra_files = files,
                     static_files = statics, threaded=True)
-
     except KeyboardInterrupt:
-        print "OK"
+        pass
 
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("-p", "--port", type="int", dest="port", default="8080",
                       help="Which port to listen")
+    parser.add_option("--mysql", dest="mysql", help="Mysql configuration: user:password@host:port")
+    parser.add_option("--storage-path", dest="storage", help="Directory used as local stoarge")
     (options, args) = parser.parse_args()
 
-    cwd = os.getcwd()
-    if cwd not in sys.path:
-        sys.path.insert(0, cwd)
-
-    try:
-        import app
-        sae.core.Application = app.Application
-    except:
-        print 'MySQL config not found: app.py'
-
-    main(cwd, options)
+    main(options)
--- dev_server/sae/core.py
+++ dev_server/sae/core.py
@@ -4,27 +4,6 @@ environ    A copy of the environ passed to your wsgi app, should not be used dir
 
 """
 
-import os
-
-import conf
-
-class Application:
-    """Information class for sae app"""
-
-    def __init__(self):
-        self.access_key = environ.get('HTTP_ACCESSKEY', '')
-        self.secret_key = environ.get('HTTP_SECRETKEY', '')
-        self.name = environ.get('HTTP_APPNAME', '')
-        self.version = environ.get('HTTP_VERSION', '')
-        self.hash = environ.get('HTTP_HASH', '')
-
-        self.mysql_db = '_'.join(['app', self.name])
-        self.mysql_user = self.access_key
-        self.mysql_pass = self.secret_key
-        self.mysql_host = conf.SAE_MYSQL_HOST_M
-        self.mysql_host_s = conf.SAE_MYSQL_HOST_S
-        self.mysql_port = conf.SAE_MYSQL_PORT
-
 def get_access_key():
     """Return access_key of your app"""
     return environ.get('HTTP_ACCESSKEY', '')
